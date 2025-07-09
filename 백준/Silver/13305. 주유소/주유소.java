953222964378496cd5be9c6ba4bcdba6df5ebb37
dnwldus410 <69383333+dnwldus410@users.noreply.github.com>
2025-07-09 23:17:38 +0900

    [Silver III] Title: 주유소, Time: 320 ms, Memory: 30552 KB, Score: 58 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -10,7 +10,7 @@ BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 int N = Integer.parseInt(br.readLine());
 int[] road = new int[N-1];
 int[] gas = new int[N];
-int result=0;
+long result=0;
 StringTokenizer st = new StringTokenizer(br.readLine());
 for(int i=0;i<N-1;i++){
     road[i] = Integer.parseInt(st.nextToken());
@@ -20,14 +20,16 @@ for(int i=0;i<N-1;i++){
             gas[i] = Integer.parseInt(st.nextToken());
         }
         int cnt=1;
+        int gascheck=0;
+        gascheck=gas[0];
        result+=gas[0]*road[0];
         for(int i=0;i<N-2;i++){
-            if(gas[i]>=gas[i+1]){
-
+            if(gascheck>=gas[i+1]){
+                gascheck=gas[i+1];
                 result+=gas[i+1]*road[cnt];
             }
             else{
-                result+=gas[i]*road[cnt];
+                result+=gascheck*road[cnt];
             }
             cnt++;
         }
