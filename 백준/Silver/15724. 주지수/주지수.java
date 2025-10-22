98b18cf266979b5e7c6b29f44f3b434549f7e446
dnwldus410 <69383333+dnwldus410@users.noreply.github.com>
2025-10-23 00:01:25 +0900

    [Silver I] Title: 주지수, Time: 664 ms, Memory: 111952 KB -BaekjoonHub

---
+++ 백준/Silver/15724. 주지수/주지수.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N;
+	static int M;
+	static int dp[][];
+	static int K;
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		dp = new int[N + 1][M + 1];
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= M; j++) {
+				int value = Integer.parseInt(st.nextToken());
+				dp[i][j] = value + dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1];
+			}
+		}
+		K = Integer.parseInt(br.readLine());
+
+		for (int k = 0; k < K; k++) {
+			st = new StringTokenizer(br.readLine());
+
+			int x1 = Integer.parseInt(st.nextToken());
+			int y1 = Integer.parseInt(st.nextToken());
+			int x2 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
+
+			sb.append(dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1]).append("\n");
+		}
+		System.out.println(sb.toString());
+	}
+}
