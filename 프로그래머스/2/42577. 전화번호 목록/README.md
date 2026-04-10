02c4737f3ff2936f51e02d2195c9046d8e38768e
dnwldus410 <69383333+dnwldus410@users.noreply.github.com>
2026-04-10 17:32:19 +0900

    [level 2] Title: 전화번호 목록, Time: 433.23 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577#) 
 
 ### 성능 요약
 
-메모리: 98.3 MB, 시간: 378.43 ms
+메모리: 109 MB, 시간: 433.23 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 13일 00:37:20
+2026년 04월 10일 17:32:16
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,14 +2,42 @@ import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
-        Arrays.sort(phone_book);
-        
-        for(int i=0;i<phone_book.length-1;i++){
-            if(phone_book[i+1].startsWith(phone_book[i])){
-                answer=false;
+        HashSet<String> set = new HashSet<>(); //동일값 안되고
+        ArrayList<Integer> list = new ArrayList<>();  //접두어 길이
+         Arrays.sort(phone_book);
+        String startnum=phone_book[0].substring(0,1);
+        list.add(phone_book[0].length());  //0값 길이 ex)119
+        set.add(phone_book[0]);             //0값 넣기 119
+       
+        int length=0;
+        for(int i=1;i<phone_book.length;i++){
+          
+            if(!answer){
+                break;
             }
-
+            set.add(phone_book[i]);
+             if(!phone_book[i].substring(0,1).equals(startnum)){
+                startnum=phone_book[i].substring(0,1);
+                list.clear();
+            }
+            
+            if(!list.contains(phone_book[i].length())){   //다음꺼 4가 없으면
+                      list.add(phone_book[i].length());    //3,4 
+                } 
+           
+        
+            for(int j=0;j<list.size();j++){  //지금 size() = 2
+                      
+                length=list.get(j);
+            
+                if(phone_book[i].length()>length){
+                     if(set.contains(phone_book[i].substring(0,length))){
+                        answer=false;
+                         break;
+                }               
+            }           
+           }                
         }
-   return answer;
+        return answer;
     }
 }
